0331d42300902574ece9db8f8ad3b5cc1f3189fc
kristijanjakimov <kiko.jakimov@gmail.com>
2024-09-16 10:07:47 +0200

    Merge complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,6 @@
 package org.example;
 
-//Step 6: Locally merge the changes from the main branch into your feature branch.
-//
-//Step 7: Resolve any conflicts during the merge.
-//
-//Step 8: Push your feature branch to GitHub.
-//
+
 //Step 9: Create a pull request for your feature branch and have your colleagues review it.
 //
 //        Step 10: After approval, perform the merge and delete the feature branch.
