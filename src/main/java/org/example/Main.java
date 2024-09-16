fcdc95793ef2eccc777acd9b87900fe3b17228d9
kristijanjakimov <kiko.jakimov@gmail.com>
2024-09-16 10:03:22 +0200

    Comments deleted

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Step 6: Locally merge the changes from the main branch into your feature branch.
+//
+//Step 7: Resolve any conflicts during the merge.
+//
+//Step 8: Push your feature branch to GitHub.
+//
+//Step 9: Create a pull request for your feature branch and have your colleagues review it.
+//
+//        Step 10: After approval, perform the merge and delete the feature branch.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.printf("Changes to the main");
     }
 }
